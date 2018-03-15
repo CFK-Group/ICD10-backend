f84041e35031b082bb3d004de9f507ecfc2e4b04
Unknown <alex.villa.v@gmail.com>
2018-03-15 15:45:59 -0300

    add Route for add campaign

--- src/routes/campaignRoutes.js
+++ src/routes/campaignRoutes.js
@@ -20,22 +20,22 @@ module.exports = function (app) {
             }
         });
     });
-    /*
-    app.post('/channels/new', (req, res) => {
+
+    app.post('/campaign/new', (req, res) => {
         global.validateToken(req.query.token, function (response, err){
             if (!err){
                 console.log(req.body);
-                channel.addChannel(req.body, function (error, data) {
+                /*channel.addChannel(req.body, function (error, data) {
                     if(!error){
                         res.status(200).json(data);
                     }else{
                         console.log('Error!!!! ', error);
                         res.status(500).send('Internal Server Error');
                     }
-                });
+                });*/
             }else{
                 res.status(500).send('Internal Server Error');
             }
         });
-    });*/
+    });
 };
